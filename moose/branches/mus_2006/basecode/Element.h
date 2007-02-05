9e9a81eba530a39d35ea098ba49a31ce9be75faa
Niraj Dudani <niraj@ncbs.res.in>
2007-02-05 18:18:25 +0000

    Moose 2007, with a cleaner API and shiny new architecture and is being checked in. An overview of new architecture can be found in 'DOCS/MOOSE2007_INTRO', and is included below.
    Moose 2007 will replace the existing trunk, while the old trunk is shifted to 
    branches/mus_2006.
    
    Compiles on Visual C++ .NET (use MusWin.sln, MusWin.vcproj) and Bloodshed 
    Dev-C++ under Windows. Passes all unit tests.
    
    --Niraj
    
    (MOOSE architecture introduction by Upi follows..)
    ================================================================================
    
    MOOSE architecture introduction
    
    31 Jan 2007
    
    Upi Bhalla
    
    Overview.
    
    The current (2007) version of MOOSE is a complete refactoring of the 2006
    version. The 2006 version was a success, in that it was able to provide 
    functionality for almost all aspects of GENESIS, including language
    backward compatibility. It was also successful in defining the range of
    functions needed to support both the old GENESIS functioning, and newer
    core features such as solvers, parallelism, and flexible messaging.
    An overview of this API is in the file MOOSE2006_API.
    
    As MOOSE2006 was being coded, it became clear that its API was getting rather
    baroque and it became harder to squeeze in new features (e.g., parallel
    messaging) within the old framework. The old API included of 36 shell
    functions, 8 message variants, 21 Field info classes (Finfos), and
    11 variants on the Connection class. In addition, every new class was 
    a doubly inherited class merging Element and the user's own class
    definition. This involved rather ugly template-based set up of messaging
    for each class. This was accomplished using a preprocessor which parsed
    yet another higher-level class definition to generate a reasonably complete
    class definition involving 3 files.
    
    MOOSE2007 implements the same functionality with much less.
    - The messaging is completely revamped, and it doesn't
    make much sense to talk about message variants now. 
    - There are currently 12 Finfo classes, but we still cannot do everything
    that MOOSE06 did. 
    - There is only one Connection class, and that is not going to expand.
    - The user-class definition is now completely separate from the Element
    definition. User class data is just a void* pointer on the Element class.
    - Due to a vast simplification of messaging, we don't need to do much
    templating during class definition. It is probably simple enough to
    completely avoid the use of any preprocessing.
    
    In addition, MOOSE2007 has several coding advances built into it from a very
    early stage:
    - Use of Doxygen for automatic documentation
    - Extensive use of assertions
    - Extensive and rigorous unit tests for the base code
    - Compilation with Windows as well as Linux.
    - Integration with SWIG as an alternative to the GENESIS parser. Currently
    	only the Python module is set up to compile.
    
    ================================================================================

---
+++ moose/branches/mus_2006/basecode/Element.h
@@ -0,0 +1,207 @@
+/**********************************************************************
+** This program is part of 'MOOSE', the
+** Messaging Object Oriented Simulation Environment,
+** also known as GENESIS 3 base code.
+**           copyright (C) 2003-2005 Upinder S. Bhalla. and NCBS
+** It is made available under the terms of the
+** GNU Lesser Public License version 2.1
+** See the file COPYING.LIB for the full notice.
+**********************************************************************/
+#ifndef _ELEMENT_H
+#define _ELEMENT_H
+
+// Utility function for finding how msgs are triggered.
+Element* traverseSrcToTick( Field& f );
+
+// Abstract base class for all Elements.
+class Element {
+	// friend class Neutral;
+	public:
+		// Core API for Element
+		Element(const std::string& name)
+			: name_(name), childIn_(this)
+		{ ; }
+
+		virtual ~Element() {
+			;
+		}
+
+		const std::string& name() const {
+			return name_;
+		}
+
+		////////////////////////////////////////////////////////////
+		// Field and class stuff.
+		////////////////////////////////////////////////////////////
+
+		// New field-modifying relays are inserted near the front so
+		// that searches for field matches are faster. 
+		// Solvers are first.
+		// virtual void AddRelay(const relay& r);
+
+		static void setName( Conn* c, std::string name ) {
+			reinterpret_cast< Element* >( c->parent() )->name_ = name;
+		}
+
+		static std::string getName( const Element* e ) {
+			return e->name_;
+		}
+
+		////////////////////////////////////////////////////////////
+		// Hierarchy and traversal stuff
+		////////////////////////////////////////////////////////////
+
+		static Element* root() {
+			return root_;
+		}
+		static Element* classes() {
+			return classes_;
+		}
+
+		virtual Element* parent() const;
+
+		const std::string path() const;
+
+		virtual Element* relativeFind( const std::string& n );
+
+		static Element* find( const std::string& absolutePath ) {
+			return root_->relativeFind( absolutePath );
+		}
+
+		// Adds child to child list. Done by Neutral, possibly we
+		// may want to specialize somewhere, so it is a virtual.
+		virtual bool adoptChild(Element *child) = 0;
+
+
+		////////////////////////////////////////////////////////////
+		// Stuff to do with relays
+		////////////////////////////////////////////////////////////
+
+		Field field( const std::string& name );
+
+		void appendRelay( Finfo* f ) {
+			relays_.push_back( f );
+		}
+
+		void dropRelay( Finfo* f ) {
+			relays_.erase( std::find( relays_.begin(), relays_.end(), f ) );
+		}
+
+		Finfo* findVacantValueRelay( Finfo* valueFinfo, bool isSending);
+
+		// looks for a Finfo that either uses (Dest) or calls (Src)
+		// the specified RecvFunc. Used to traverse messages.
+		Field lookupSrcField( Conn*, RecvFunc func );
+		Field lookupDestField( Conn*, RecvFunc func );
+
+		// This virtual function checks if the element is solved,
+		// in which case it triggers the solver to do an update of
+		// the field value after checking that the field is one
+		// which depends on the solver. The SolverOp tells the
+		// solver what the operation was: set, get, addmsg, deletemsg..
+		// By default it does nothing.
+		virtual void solverUpdate( const Finfo* f, SolverOp s ) const {
+			return;
+		}
+		// Helper function
+		virtual bool isSolved() const {
+				return 0;
+		}
+
+///////////////////////////////////////////////////////////////////////
+// These boilerplate functions are used to access the object.
+///////////////////////////////////////////////////////////////////////
+		virtual const Cinfo* cinfo() const {
+			return &cinfo_;
+		}
+
+		static Element* create(
+			const std::string& n, Element* pa, const Element* proto) {
+			return 0;
+		}
+
+		virtual RecvFunc getDeleteFunc() {
+			return deleteFunc;
+		}
+
+		// Seems paradoxical, but when the message from parent to 
+		// child is triggered, it means that the child should delete
+		// itself.
+		static void deleteFunc( Conn* c ) {
+			delete c->parent();
+		}
+
+		static Conn* getChildIn( Element* e ) {
+			return &( e->childIn_ );
+		}
+
+		void listFields( std::vector< Finfo* >& );
+
+///////////////////////////////////////////////////////////////////////
+// Wildcarding Functions 
+///////////////////////////////////////////////////////////////////////
+		// These are defined in Wildcard.cpp
+		static int startFind( const std::string& n, std::vector< Element* >& ret);
+		Element* wildcardName(const std::string& n ) ;
+        virtual int wildcardRelativeFind(
+			const std::string& n, std::vector< Element* >& ret, int doublehash) ;
+		static int wildcardFind(
+			const std::string& path, std::vector< Element* >& ret );
+		Element* wildcardFieldComparison( const std::string& line );
+
+///////////////////////////////////////////////////////////////////////
+// Functions involved in different kinds of copying
+///////////////////////////////////////////////////////////////////////
+		virtual Element* shallowCopy( Element* parent ) const;
+
+		// Used to do any special operations, such as rebuild an
+		// inner representation of other elements, after a copy.
+		// Called on the copied object. Used for solvers.
+		virtual void handlePostCopy( 	)
+		{
+			;
+		}
+
+		// This is the regular copy. Copies an entire element tree
+		// including all messages within the tree. Returns the
+		// copied Element
+		Element* deepCopy( Element* pa ) const;
+
+		// Checks if pa is anywhere in the ancestry of current element
+		bool descendsFrom( const Element* pa ) const;
+
+		// The final part of the copy routine.
+		// This refers to each object class to traverse children.
+		// Returns copied object
+		virtual Element* internalDeepCopy( Element* pa, 
+			std::map< const Element*, Element* >& tree ) const;
+	protected:
+		static std::string* nameValFunc(Element* e) {
+			return &(e->name_);
+		}
+
+		// This does the actual work of the copying, using the tree
+		// to handle the message copying
+		// Returns copied object
+		Element* deepTreeCopy( Element* pa, 
+			std::map< const Element*, Element* >& tree ) const;
+
+		// Used in copying
+		// was Defined in MsgFuncs.cpp
+		void duplicateMessagesOnTree(std::map<const Element*, Element*>& tree) const;
+
+	private:
+		std::string name_;
+	//	UniConn< Element, Element::childIn_ > childIn_;
+	//	UniConn< Element, childIn_ > childIn_;
+	//	static Element* lookupChildIn(const Conn *);
+		UniConn2 childIn_;
+		std::vector< Finfo* > relays_;
+		static Element* root_;
+		static Element* classes_;
+		static Element* initializeRoot();
+		static Finfo* fieldArray_[];
+		static const Cinfo cinfo_;
+};
+
+#endif	// _ELEMENT_H
