cd3f18d3fa2be98feb1929516f948112fa7b27fc
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2007-09-27 04:27:33 +0000

    Added configuration handling and initializaer. Bypassing autoscheduling not yet implemented.

---
+++ moose/branches/subha_moose/utility/Utility.h
@@ -0,0 +1,8 @@
+/*******************************************************************
+ * File:            Utility.h
+ * Description:     This is a set of utility functions for MOOSE.
+ * Author:          Subhasis Ray
+ * E-mail:          ray.subhasis@gmail.com
+ * Created:         2007-09-25 08:18:20
+ ********************************************************************/
+
