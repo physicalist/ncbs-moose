f5bb47835e564edadc296cdf23c5f373cde4644e
Niraj Dudani <niraj@ncbs.res.in>
2008-08-12 13:22:12 +0000

    (branch: branching_hsolve) Preparatory work for the HSolvePassive unit test. Sets up a small cell model locally, and compares matrices generated by the solver, and "by hand".
    Added a function in the utility folder, to compare floating point numbers.

--- moose/branches/branching_hsolve/biophysics/TestHSolve.cpp
+++ moose/branches/branching_hsolve/biophysics/TestHSolve.cpp
@@ -9,15 +9,95 @@
 **********************************************************************/
 #ifdef DO_UNIT_TESTS
 
-#include "header.h"
+#include <vector>
+using namespace std;
 
-extern void testHinesMatrix(); // Defined in HinesMatrix.cpp
 extern void testBioScan(); // Defined in BioScan.cpp
+extern void testHinesMatrix(); // Defined in HinesMatrix.cpp
+extern void testHSolvePassive(); // Defined in HSolvePassive.cpp
 
 void testHSolve()
 {
-	testHinesMatrix();
 	testBioScan();
+	testHinesMatrix();
+	testHSolvePassive();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Helper functions called in testHinesMatrix, testHSolvePassive, etc.
+//////////////////////////////////////////////////////////////////////////////
+
+void array2vec(
+	int *array,
+	int size,
+	vector< vector< unsigned int > >& vec )
+{
+	for ( int i = 0; i < size; i++ )
+		if ( array[ i ] == -1 )
+			vec.resize( vec.size() + 1 );
+		else		
+			vec.back().push_back( array[ i ] );
+}
+
+void makeFullMatrix(
+	unsigned int size,
+	const vector< vector< unsigned int > >& children,
+	const vector< vector< unsigned int > >& coupling,
+	const vector< double >& Ga,
+	const vector< double >& CmByDt,
+	vector< vector< double > >& matrix )
+{
+	matrix.resize( size );
+	for ( unsigned int i = 0; i < size; ++i )
+		matrix[ i ].resize( size );
+	
+	// Setting diagonal elements
+	for ( unsigned int i = 0; i < size; i++ )
+		matrix[ i ][ i ] = CmByDt[ i ];
+	
+	double gi;
+	vector< vector< unsigned int > >::const_iterator group;
+	vector< unsigned int >::const_iterator ic;
+	for ( group = coupling.begin(); group != coupling.end(); ++group ) {
+		double gsum = 0.0;
+		
+		for ( ic = group->begin(); ic != group->end(); ++ic )
+			gsum += Ga[ *ic ];
+		
+		for ( ic = group->begin(); ic != group->end(); ++ic ) {
+			gi = Ga[ *ic ];
+			
+			matrix[ *ic ][ *ic ] += gi * ( 1.0 - gi / gsum );
+		}
+	}
+	
+	// Setting off-diagonal elements
+	double gij;
+	vector< unsigned int >::const_iterator jc;
+	for ( group = coupling.begin(); group != coupling.end(); ++group ) {
+		double gsum = 0.0;
+		
+		for ( ic = group->begin(); ic != group->end(); ++ic )
+			gsum += Ga[ *ic ];
+		
+		for ( ic = group->begin(); ic != group->end() - 1; ++ic ) {
+			for ( jc = ic + 1; jc != group->end(); ++jc ) {
+				gij = Ga[ *ic ] * Ga[ *jc ] / gsum;
+				
+				matrix[ *ic ][ *jc ] = -gij;
+				matrix[ *jc ][ *ic ] = -gij;
+			}
+		}
+	}
+}
+
+void updateIndices(
+	vector< vector< unsigned int > >& v,
+	const vector< unsigned int >& permutation )
+{
+	for ( unsigned int i = 0; i < v.size(); i++ )
+		for ( unsigned int j = 0; j < v[ i ].size(); j++ )
+			v[ i ][ j ] = permutation[ v[ i ][ j ] ];
 }
 
 #endif
