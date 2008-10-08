cbb2a25bcec8aad368518dafad31308fd794b911
niraj_ <niraj_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2008-10-08 09:23:25 +0000

    Intermediate commit

--- moose/branches/music/maindir/init.cpp
+++ moose/branches/music/maindir/init.cpp
@@ -63,9 +63,9 @@ int mooseInit()
     // These two should really be solver managers because there are
     // a lot of decisions to be made about how the simulation is best
     // solved. For now let the Shell deal with it.
-    Neutral::create( "Neutral", "chem", solvers->id(), Id::scratchId() );
-    Neutral::create( "Neutral", "neuronal", solvers->id(), Id::scratchId() );
-
+    // Neutral::create( "Neutral", "chem", solvers->id(), Id::scratchId() );
+    // Neutral::create( "Neutral", "neuronal", solvers->id(), Id::scratchId() );
+	
     // Not really honouring AUTOSCHEDULE setting -
     // May need only t0 for AUTOSCHEDULE=false
     // But creating a few extra clock ticks does not hurt as much as
