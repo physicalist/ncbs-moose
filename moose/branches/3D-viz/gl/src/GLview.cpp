9ec565fdc903528ef167b1d91ab06faff481366e
karanvasudeva <karanvasudeva@users.noreply.github.com>
2009-10-28 07:00:53 +0000

    GLview: added code to grab field values with the equivalents of 'path','relpath' and 'field' in xview

--- moose/branches/3D-viz/gl/src/GLview.cpp
+++ moose/branches/3D-viz/gl/src/GLview.cpp
@@ -8,7 +8,9 @@
 **********************************************************************/
 
 #include "moose.h"
-#include "../shell/Shell.h"
+#include "shell/Shell.h"
+#include "element/Wildcard.h"
+
 #include "GLview.h"
 
 #include <iostream>
@@ -40,6 +42,36 @@ const Cinfo* initGLviewCinfo()
 				ValueFtype1< string >::global(),
 				GFCAST( &GLview::getPath ),
 				RFCAST( &GLview::setPath )
+				),
+		new ValueFinfo( "relpath",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getRelPath ),
+				RFCAST( &GLview::setRelPath )
+				),
+		new ValueFinfo( "value1",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getValue1Field ),
+				RFCAST( &GLview::setValue1Field )
+				),
+		new ValueFinfo( "value2",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getValue2Field ),
+				RFCAST( &GLview::setValue2Field )
+				),
+		new ValueFinfo( "value3",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getValue3Field ),
+				RFCAST( &GLview::setValue3Field )
+				),
+		new ValueFinfo( "value4",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getValue4Field ),
+				RFCAST( &GLview::setValue4Field )
+				),
+		new ValueFinfo( "value5",
+				ValueFtype1< string >::global(),
+				GFCAST( &GLview::getValue5Field ),
+				RFCAST( &GLview::setValue5Field )
 				),
 	///////////////////////////////////////////////////////
 	// Shared definitions
@@ -79,12 +111,38 @@ static const Cinfo* glviewCinfo = initGLviewCinfo();
 
 GLview::GLview()
 	:
-	strPath_( "" )
+	strPath_( "" ),
+	strRelPath_( "" ),
+	values1_( NULL ),
+	values2_( NULL ),
+	values3_( NULL ),
+	values4_( NULL ),
+	values5_( NULL ),
+	strValue1Field_( "" ),
+	strValue2Field_( "" ),
+	strValue3Field_( "" ),
+	strValue4Field_( "" ),
+	strValue5Field_( "" ),
+	x_( NULL ),
+	y_( NULL ),
+	z_( NULL )
 {
 }
+
 GLview::~GLview()
 {
-  // disconnect(); // TODO karan
+	// disconnect(); // TODO karan
+	
+	if ( values1_ != NULL )
+		free( values1_ );
+	if ( values2_ != NULL )
+		free( values2_ );
+	if ( values3_ != NULL )
+		free( values3_ );
+	if ( values4_ != NULL )
+		free( values4_ );
+	if ( values5_ != NULL )
+		free( values5_ );
 }
 
 ///////////////////////////////////////////////////
@@ -103,7 +161,97 @@ void GLview::innerSetPath( const string& strPath )
 
 string GLview::getPath( Eref e )
 {
-	return static_cast< const GLview* >( e.data() )->strPath_;
+	return static_cast< const GLview * >( e.data() )->strPath_;
+}
+
+void GLview::setRelPath( const Conn* c, string strRelPath )
+{
+	static_cast< GLview * >( c->data() )->innerSetRelPath( strRelPath );
+}
+
+void GLview::innerSetRelPath( const string& strRelPath )
+{
+	strRelPath_ = strRelPath;
+}
+
+string GLview::getRelPath( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strRelPath_;
+}
+
+void GLview::setValue1Field( const Conn* c, string strValue1Field )
+{
+	static_cast< GLview * >( c->data() )->innerSetValue1Field( strValue1Field );
+}
+
+void GLview::innerSetValue1Field( const string& strValue1Field )
+{
+	strValue1Field_ = strValue1Field;
+}
+
+string GLview::getValue1Field( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strValue1Field_;
+}
+
+void GLview::setValue2Field( const Conn* c, string strValue2Field )
+{
+	static_cast< GLview * >( c->data() )->innerSetValue2Field( strValue2Field );
+}
+
+void GLview::innerSetValue2Field( const string& strValue2Field )
+{
+	strValue2Field_ = strValue2Field;
+}
+
+string GLview::getValue2Field( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strValue2Field_;
+}
+
+void GLview::setValue3Field( const Conn* c, string strValue3Field )
+{
+	static_cast< GLview * >( c->data() )->innerSetValue3Field( strValue3Field );
+}
+
+void GLview::innerSetValue3Field( const string& strValue3Field )
+{
+	strValue3Field_ = strValue3Field;
+}
+
+string GLview::getValue3Field( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strValue3Field_;
+}
+
+void GLview::setValue4Field( const Conn* c, string strValue4Field )
+{
+	static_cast< GLview * >( c->data() )->innerSetValue4Field( strValue4Field );
+}
+
+void GLview::innerSetValue4Field( const string& strValue4Field )
+{
+	strValue4Field_ = strValue4Field;
+}
+
+string GLview::getValue4Field( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strValue4Field_;
+}
+
+void GLview::setValue5Field( const Conn* c, string strValue5Field )
+{
+	static_cast< GLview * >( c->data() )->innerSetValue5Field( strValue5Field );
+}
+
+void GLview::innerSetValue5Field( const string& strValue5Field )
+{
+	strValue5Field_ = strValue5Field;
+}
+
+string GLview::getValue5Field( Eref e )
+{
+	return static_cast< GLview * >( e.data() )->strValue5Field_;
 }
 
 ///////////////////////////////////////////////////
@@ -117,7 +265,23 @@ void GLview::reinitFunc( const Conn* c, ProcInfo info )
 
 void GLview::reinitFuncLocal( const Conn* c )
 {
-	std::cout << "in GLview reinit" << std::endl;
+	elements_.clear();
+
+	wildcardFind( strPath_, elements_ );
+	std::cout << "GLview: " << elements_.size() << " elements found." << std::endl; // TODO comment this out by default
+
+	if ( ! strValue1Field_.empty() )
+		populateValues( 1, values1_, strValue1Field_ ); 
+	if ( ! strValue2Field_.empty() )
+		populateValues( 2, values2_, strValue2Field_ ); 
+	if ( ! strValue3Field_.empty() )
+		populateValues( 3, values3_, strValue3Field_ ); 
+	if ( ! strValue4Field_.empty() )
+		populateValues( 4, values4_, strValue4Field_ ); 
+	if ( ! strValue5Field_.empty() )
+		populateValues( 5, values5_, strValue5Field_ ); 
+	
+	// TODO must get x,y,z in reinitFuncLocal
 }
 
 void GLview::processFunc( const Conn* c, ProcInfo info )
@@ -133,6 +297,58 @@ void GLview::processFuncLocal( Eref e, ProcInfo info )
 ///////////////////////////////////////////////////
 // private function definitions
 ///////////////////////////////////////////////////
+
+int GLview::populateValues( int valueNum, double * values, string strValueField )
+{
+	int status = 0;
+
+	if ( values == NULL)
+		values = ( double * ) malloc( sizeof( double ) * elements_.size() );
+
+	if ( values == NULL )
+	{
+		std::cerr << "GLview error: could not allocate memory to set field values" << std::endl;
+		status = -1;
+	}
+	else
+	{
+		for ( unsigned int i = 0; i < elements_.size(); ++i)
+		{
+			Id id = elements_[i];
+			string path;
+
+			if ( ! strRelPath_.empty() ) 
+			{
+				path = elements_[i].path();
+				path.push_back('/');
+				path.append(strRelPath_);
+				id = Id::Id( path, "/" );
+			}
+			
+			if ( id.eref() == NULL )
+			{
+				std::cerr << "Could not find path: " << path << "; error in relpath? " << std::endl;
+				status = -2;
+				break;
+			}
+			else if ( ! get< double >( id.eref(), strValueField, values[i] ) )
+			{
+				std::cerr << "GLview error: for value" << valueNum << ", unable to find a field called '" << strValueField << "' in elements on the path " << strPath_ << strRelPath_ << std::endl;
+				status = -3;
+				break;
+			}
+			/* else
+			   {
+			   std::cout << "field1 value " << strValueField << " for element " << i << " is " << values[i] << std::endl;
+			   } */
+		}
+	}
+
+	if ( status == -2 || status == -3 )
+		free( values );
+
+	return status;
+}
 
 ///////////////////////////////////////////////////
 // networking helper function definitions
--- moose/branches/3D-viz/gl/src/GLview.h
+++ moose/branches/3D-viz/gl/src/GLview.h
@@ -7,6 +7,8 @@
 ** See the file COPYING.LIB for the full notice.
 **********************************************************************/
 
+#include "moose.h"
+
 enum MSGTYPE
 {
 	RESET,
@@ -32,11 +34,56 @@ class GLview
 	static void setPath( const Conn* c, string strPath );
 	void innerSetPath( const string& strPath );
 	static string getPath( Eref e );
+	
+	static void setRelPath( const Conn* c, string strRelPath );
+	void innerSetRelPath( const string& strRelPath );
+	static string getRelPath( Eref e );
 
+	static void setValue1Field( const Conn* c, string strValue1Field );
+	void innerSetValue1Field( const string& strValue1Field );
+	static string getValue1Field( Eref e );
+
+	static void setValue2Field( const Conn* c, string strValue2Field );
+	void innerSetValue2Field( const string& strValue2Field );
+	static string getValue2Field( Eref e );
+	
+	static void setValue3Field( const Conn* c, string strValue3Field );
+	void innerSetValue3Field( const string& strValue3Field );
+	static string getValue3Field( Eref e );
+	
+	static void setValue4Field( const Conn* c, string strValue4Field );
+	void innerSetValue4Field( const string& strValue4Field );
+	static string getValue4Field( Eref e );
+	
+	static void setValue5Field( const Conn* c, string strValue5Field );
+	void innerSetValue5Field( const string& strValue5Field );
+	static string getValue5Field( Eref e );
+	
 	static const int MSGTYPE_HEADERLENGTH;
      	static const int MSGSIZE_HEADERLENGTH;
 	static const char SYNCMODE_ACKCHAR;
 
  private:
 	string strPath_;
+	string strRelPath_;
+
+	vector< Id > elements_; // the elements found on strPath_
+
+	double *values1_;
+	double *values2_;
+	double *values3_;
+	double *values4_;
+	double *values5_;
+	string strValue1Field_;
+	string strValue2Field_;
+	string strValue3Field_;
+	string strValue4Field_;
+	string strValue5Field_;
+
+	double *x_;
+	double *y_;
+	double *z_;
+
+	int populateValues( int valueNum, double * values, string strValueField );
+  
 };
