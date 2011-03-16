d6b5c6c8810858eb420471dc5f737a326bab7ccd
subha_ <subha_@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2011-03-16 12:29:40 +0000

    Got a working version of Neutral wrapper.

--- moose/branches/pymoose2/moose.py
+++ moose/branches/pymoose2/moose.py
@@ -7,9 +7,9 @@
 # Copyright (C) 2010 Subhasis Ray, all rights reserved.
 # Created: Sat Mar 12 14:02:40 2011 (+0530)
 # Version: 
-# Last-Updated: Tue Mar 15 16:44:01 2011 (+0530)
+# Last-Updated: Wed Mar 16 17:59:22 2011 (+0530)
 #           By: Subhasis Ray
-#     Update #: 31
+#     Update #: 44
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -32,7 +32,7 @@
 import _moose
 
 class Neutral(object):
-    def __init__(self, path, dims=[1]):
+    def __init__(self, path, dims=[1], className='Neutral'):
         if isinstance(dims, int):
             dims = [dims]
         else:
@@ -40,16 +40,19 @@ class Neutral(object):
                 it = iter(dims)
             except TypeError:
                 dims = [1]
-        self.__dont_touch_me = _moose._pymoose_Neutral_new('Neutral', path, dims)
+        self.__ptr = _moose._pymoose_Neutral_new(className, path, dims)
 
     # def __del__(self):
-    #     _moose._pymoose_Neutral_delete(self.__dont_touch_me)
+    #     _moose._pymoose_Neutral_delete(self.__ptr)
     def __get_id(self):
-        return _moose._pymoose_Neutral_id(self.__dont_touch_me)
-    def __get_path(self):
-        return _moose._pymoose_Neutral_path(self.__dont_touch_me)
+        return _moose._pymoose_Neutral_id(self.__ptr)
     id = property(__get_id) # moose Id
+    def __get_path(self):
+        return _moose._pymoose_Neutral_path(self.__ptr)
     path = property(__get_path)
+    def __get_class(self):
+        return _moose._pymoose_Neutral_getattr(self.__ptr, 'class', 'string')
+    className = property(__get_class)
         
 # 
 # moose.py ends here
