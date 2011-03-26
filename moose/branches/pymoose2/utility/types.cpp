fafdf70dca9caffd4b155b804b0718218ab29162
Subhasis Ray <subhasis@ncbs.res.in>
2011-03-26 14:04:22 +0000

    Switching to use of PyTypeObject in order to make the wrappers appear to be builtin types to Python. Compiles but does not work. More coding needed to resolve the funny hierarchy of Id-ObjId-DataId in a less confusing manner.

--- moose/branches/pymoose2/utility/types.cpp
+++ moose/branches/pymoose2/utility/types.cpp
@@ -7,9 +7,9 @@
 // Copyright (C) 2010 Subhasis Ray, all rights reserved.
 // Created: Wed Mar 23 10:10:45 2011 (+0530)
 // Version: 
-// Last-Updated: Wed Mar 23 17:04:53 2011 (+0530)
+// Last-Updated: Sat Mar 26 18:18:11 2011 (+0530)
 //           By: Subhasis Ray
-//     Update #: 41
+//     Update #: 44
 // URL: 
 // Keywords: 
 // Compatibility: 
@@ -40,9 +40,9 @@ char shortType(string name)
     if (typemap.empty()){
         typemap.insert(pair<string, char>("char", 'c'));
         typemap.insert(pair<string, char>("int", 'i'));
-        typemap.insert(pair<string, char>("short", 'j'));
+        typemap.insert(pair<string, char>("short", 'h'));
         typemap.insert(pair<string, char>("long", 'l'));
-        typemap.insert(pair<string, char>("unsigned int", 'u'));
+        typemap.insert(pair<string, char>("unsigned int", 'I'));
         typemap.insert(pair<string, char>("unsigned long", 'k'));        
         typemap.insert(pair<string, char>("float", 'f'));
         typemap.insert(pair<string, char>("double", 'd'));
@@ -57,7 +57,7 @@ char shortType(string name)
         typemap.insert(pair<string, char>("vector<unsigned int>", 'U'));
         typemap.insert(pair<string, char>("vector<unsigned long>", 'K'));        
         typemap.insert(pair<string, char>("vector<float>", 'F'));
-        typemap.insert(pair<string, char>("vector<double>", 'x'));
+        typemap.insert(pair<string, char>("vector<double>", 'D'));
         typemap.insert(pair<string, char>("vector<string>", 'S'));
         typemap.insert(pair<string, char>("vector<Id>", 'J'));
         typemap.insert(pair<string, char>("vector<ObjId>", 'P'));
