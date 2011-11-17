3d77c0ea347dbce7dfbbcdc4f45b06ce7692845c
Subhasis Ray <subhasis@ncbs.res.in>
2011-11-17 05:02:51 +0000

    Updated moosestart.py to reflect Siji's updated moosegui script.

--- moose/branches/moose_Beta_1.4/gui/moosestart.py
+++ moose/branches/moose_Beta_1.4/gui/moosestart.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # moosestart.py --- 
 # 
 # Filename: moosestart.py
@@ -6,9 +7,9 @@
 # Maintainer: 
 # Created: Wed Nov 16 20:08:34 2011 (+0530)
 # Version: 
-# Last-Updated: Wed Nov 16 21:12:05 2011 (+0530)
+# Last-Updated: Thu Nov 17 10:31:19 2011 (+0530)
 #           By: Subhasis Ray
-#     Update #: 74
+#     Update #: 104
 # URL: 
 # Keywords: 
 # Compatibility: 
@@ -40,6 +41,26 @@ moose_version = '1.4'
 
 if __name__ == '__main__':
     moose_dir = os.path.join(sys.path[0], '..')    
+    # The following code is for copying DEMOS and TESTS to user's home
+    # directory is not already present.
+    user_home = os.path.expanduser('~')
+    user_moose_conf_dir = os.path.join(user_home, '.moose')
+    user_moose_conf_file = os.path.join(user_moose_conf_dir, 'moose%s' % (moose_version))
+    user_moose_dir = os.path.join(user_home, 'moose%s' % (moose_version))    
+    symlinkpath = os.path.join(user_home, 'Desktop', 'moose%s' % (moose_version))
+    if not os.path.lexists(user_moose_conf_dir):
+        print 'Creating moose configuration directory:', user_moose_conf_dir
+        os.mkdir(user_moose_conf_dir, 0755)
+        fd = open(user_moose_conf_file, 'w')
+        fd.write('# moose%s' % moose_version)
+        fd.close()
+    if not os.path.lexists(user_moose_dir):
+        print 'Creating local moose directory:', user_moose_dir
+        os.mkdir(user_moose_dir, 0755)
+        copy_tree(os.path.join(moose_dir, 'DEMOS'), os.path.join(user_moose_dir, 'DEMOS'))
+        copy_tree(os.path.join(moose_dir, 'TESTS'), os.path.join(user_moose_dir, 'TESTS'))
+        print 'Copied DEMOS and TESTS to local moose directory'
+
     if platform.system() == 'Linux' or platform.system() == 'Darwin':
         ld_library_path = os.path.join(moose_dir, 'lib')
         try:
@@ -47,23 +68,11 @@ if __name__ == '__main__':
         except KeyError:
             pass
         os.environ['LD_LIBRARY_PATH'] = ld_library_path
-    # The following code is for copying DEMOS and TESTS to user's home
-    # directory is not already present.
-    user_home = os.path.expanduser('~')
-    user_moose_conf_dir = os.path.join(user_home, '.moose')
-    user_moose_conf_file = os.path.join(user_moose_conf_dir, 'moose%s' % (moose_version))
-    user_moose_dir = os.path.join(user_home, 'moose%s' % (moose_version))    
-    if not os.path.lexists(user_moose_conf_dir):
-        print 'Creating moose configuration directory:', user_moose_conf_dir
-        os.mkdir(user_moose_conf_dir, 0755)
-        fd = os.open(user_moose_conf_file, os.O_WRONLY)
-        os.close(fd)
-    if not os.path.lexists(user_moose_dir):
-        print 'Creating local moose directory:', user_moose_dir
-        os.mkdir(user_moose_dir, 0755)
-        copy_tree(os.path.join(moose_dir, 'DEMOS'), os.path.join(user_moose_dir, 'DEMOS'))
-        copy_tree(os.path.join(moose_dir, 'TESTS'), os.path.join(user_moose_dir, 'TESTS'))
-        print 'Copied DEMOS and TESTS to local moose directory'
+        python_module_path = '/usr/lib/python%d.%d/dist-packages' % (sys.version_info[0], sys.version_info[1])
+        if python_module_path not in sys.path:
+            sys.path.append(python_module_path)
+        if not os.path.lexists(symlinkpath):
+            os.symlink(user_moose_dir, symlinkpath)
     # finally run the gui.
     main(sys.argv)
     
