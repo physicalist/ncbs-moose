39eb3702cf7d712b200f17e9ee6f58263cc46e7c
pritishjetley <pritishjetley@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-11-20 11:57:21 +0000

    checking in files for charm++ test driver

---
+++ moose/branches/charm/charm_test/Main.h
@@ -0,0 +1,41 @@
+#include "ccs-client.h"
+#include "test.decl.h"
+#include "pup.h"
+
+#include "CcsId.h"
+#include "CcsDataId.h"
+#include "CcsObjId.h"
+#include "ShellProxy.h"
+#include "SetGetCcsClient.h"
+
+#include "MooseParams.h"
+
+class ProcInfo;
+class Qinfo;
+class Id;
+class ObjId;
+class DataId;
+class Element;
+class FuncId;
+class Eref;
+class Cinfo;
+#include "../msg/Msg.h"
+
+#include <iostream>
+using namespace std;
+
+
+class Main : public CBase_Main {
+  ShellProxy shell_;
+  CkArgMsg *argMsg_;
+
+  public:
+  Main(CkArgMsg *m);
+  void commence();
+
+  private:
+  void test();
+  void rtHHNetwork(ShellProxy *shell);
+};
+
+
