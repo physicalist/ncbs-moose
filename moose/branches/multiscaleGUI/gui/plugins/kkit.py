c56eedaa0627a8e9c329956a2d4f611fe5686615
hrani <hrani@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2012-12-14 06:17:15 +0000

    Genesis model loads and all moose object with the compartment are put on the screen, but without the connection. To test one has to edit mgui.py and list.txt

--- moose/branches/multiscaleGUI/gui/plugins/kkit.py
+++ moose/branches/multiscaleGUI/gui/plugins/kkit.py
@@ -1,8 +1,15 @@
 import sys
+import os
+import numpy as np
+import config
+import pickle 
 sys.path.append('plugins')
 from default import *
-
+from moose import *
 from mplugin import *
+from kkitUtil import *
+from kkitQGraphics import PoolItem, ReacItem,EnzItem,CplxItem,ComptItem
+from kkitViewcontrol import *
 class KkitPlugin(MoosePlugin):
     """Default plugin for MOOSE GUI"""
     def __init__(self, *args):
@@ -64,4 +71,178 @@ class  KineticsWidget(EditorWidgetBase):
         #print "KKIT plugin",self.modelRoot
         
     def updateModelView(self):
-        print "Here in kkit plugin overridden updatemodelview from EditorWidgetBase and  modelroot path",self.modelRoot
+        """ maxmium and minimum coordinates of the objects specified in kkit file. """
+        self.xmin = 0.0
+        self.xmax = 1.0
+        self.ymin = 0.0
+        self.ymax = 1.0
+        self.size = QtCore.QSize(1024 ,768)
+
+        """ pickled the color map file """
+        colormap_file = open(os.path.join(config.settings[config.KEY_COLORMAP_DIR], 'rainbow2.pkl'),'rb')
+        self.colorMap = pickle.load(colormap_file)
+        colormap_file.close()
+        
+        self.setupMeshObj()
+        #for key,value in self.meshEntry.items():             print key,value
+            
+        if self.noPositionInfo:
+            QtGui.QMessageBox.warning(self, 
+                                      'No coordinates found', 
+                                      'Kinetic layout works only \
+for models using kkit8 or later')
+            raise Exception('Unsupported kkit version')
+        
+        """Scale factor to translate the x -y position from kkit to Qt coordinates. \
+           Qt origin is at the top-left corner. The x values increase to the right and the y values increase downwards \
+             as compared to Genesis codinates y value is upwards """
+
+        if self.xmax-self.xmin != 0:
+            self.xratio = (self.size.width()-10)/(self.xmax-self.xmin)
+        else: self.xratio = self.size.width()-10
+
+
+        if self.ymax-self.ymin:
+            self.yratio =- (self.size.height()-10)/(self.ymax-self.ymin)
+        else: self.yratio =- (self.size.height()-10)
+
+        #A map b/w moose compartment key with QGraphicsObject
+        self.qGraCompt = {}
+        
+        #A map between mooseId of all the mooseObject (except compartment) with QGraphicsObject
+        self.mooseId_GObj = {}
+
+        self.border = 5
+
+        hLayout = QtGui.QGridLayout(self)
+        self.setLayout(hLayout)
+        self.sceneContainer = QtGui.QGraphicsScene(self)
+        self.sceneContainer.setSceneRect(self.sceneContainer.itemsBoundingRect())
+        self.sceneContainer.setBackgroundBrush(QtGui.QColor(230,220,219,120))
+        self.view = GraphicalView(self.sceneContainer,self.border,self)
+        
+        """ Compartment and its members are put on the qgraphicsscene """
+        self.mooseObjOntoscene()
+        hLayout.addWidget(self.view)
+        #self.view.fitInView(self.sceneContainer.itemsBoundingRect().x()-10,self.sceneContainer.itemsBoundingRect().y()-10,self.sceneContainer.itemsBoundingRect().width()+20,self.sceneContainer.itemsBoundingRect().height()+20,Qt.Qt.IgnoreAspectRatio)
+
+    def mooseObjOntoscene(self):
+        """  All the compartments are put first on to the scene \
+             Need to do: Check With upi if empty compartments exist """
+        for cmpt in self.meshEntry.keys():
+            self.createCompt(cmpt)
+            comptRef = self.qGraCompt[cmpt]
+        
+        """ Enzymes of all the compartment are placed after compartment, \
+             so that when cplx queries for parent it gets it parent enz co-ordinates with respect to QGraphicsscene """
+        
+        for cmpt,memb in self.meshEntry.items():
+            for enzObj in self.find_index(memb,'enzyme'):
+                enzinfo = enzObj.path+'/info'
+                enzItem = EnzItem(enzObj,self.qGraCompt[cmpt])
+                self.setupDisplay(enzinfo,enzItem,"enzyme")
+                self.mooseId_GObj[element(enzObj).getId()] = enzItem
+
+        for cmpt,memb in self.meshEntry.items():
+            for poolObj in self.find_index(memb,'pool'):
+                poolinfo = poolObj.path+'/info'
+                poolItem = PoolItem(poolObj,self.qGraCompt[cmpt])
+                self.setupDisplay(poolinfo,poolItem,"pool")
+                self.mooseId_GObj[element(poolObj).getId()] = poolItem
+
+            for cplxObj in self.find_index(memb,'cplx'):
+                cplxinfo = (cplxObj[0].parent).path+'/info'
+                cplxItem = CplxItem(cplxObj,self.mooseId_GObj[element(cplxObj[0]).parent.getId()])
+                self.setupDisplay(cplxinfo,cplxItem,"cplx")
+                self.mooseId_GObj[element(cplxObj).getId()] = cplxItem
+                
+            for reaObj in self.find_index(memb,'reaction'):
+                reainfo = reaObj.path+'/info'
+                reaItem = ReacItem(reaObj,self.qGraCompt[cmpt])
+                self.setupDisplay(reainfo,reaItem,"reaction")
+                self.mooseId_GObj[element(reaObj).getId()] = reaItem
+
+
+
+        ''' compartment's rectangle size is calculated depending on children '''
+        for k, v in self.qGraCompt.items():
+            rectcompt = v.childrenBoundingRect()
+            v.setRect(rectcompt.x()-10,rectcompt.y()-10,(rectcompt.width()+20),(rectcompt.height()+20))
+            v.setPen(QtGui.QPen(Qt.QColor(66,66,66,100), 5, Qt.Qt.SolidLine, Qt.Qt.RoundCap, Qt.Qt.RoundJoin))
+
+    def setupDisplay(self,info,graphicalObj,objClass):
+        x = float(element(info).getField('x'))
+        y = float(element(info).getField('y'))
+        xpos = x*self.xratio
+        ypos = y*self.yratio
+        """ For Reaction and Complex object I have skiped the process to get the facecolor and background color as \
+            we are not using these colors for displaying the object so just passing dummy color white """
+        if( (objClass == "reaction" ) or (objClass == "cplx")):
+            textcolor,bgcolor = "",""
+        else:
+            textcolor,bgcolor = getColor(info,self.colorMap)
+
+        graphicalObj.setDisplayProperties(xpos,ypos,textcolor,bgcolor)
+
+    
+    def createCompt(self,key):
+        self.new_Compt = ComptItem(self,0,0,0,0,key)
+        self.qGraCompt[key] = self.new_Compt
+        self.new_Compt.setRect(10,10,10,10)
+        self.sceneContainer.addItem(self.new_Compt)
+        
+    def find_index(self,value, key):
+        """ Value.get(key) to avoid expection which would raise if empty value in dictionary for a given key """
+        if value.get(key) != None:
+            return value.get(key)
+        else:
+            raise ValueError('no dict with the key found')
+
+    def setupMeshObj(self):
+        ''' Setup compartment and its members pool,reaction,enz cplx under self.meshEntry dictionaries \ 
+            self.meshEntry with "key" as compartment, 
+            value is key2:list where key2 represents moose object type,list of objects of a perticular type
+            e.g self.meshEntry[meshEnt] = { 'reaction': reaction_list,'enzyme':enzyme_list,'pool':poollist,'cplx': cplxlist }
+        '''
+        self.meshEntry = {}
+        xcord = []
+        ycord = []
+        meshEntryWildcard = self.modelRoot+'/##[TYPE=MeshEntry]'
+        for meshEnt in wildcardFind(meshEntryWildcard):
+            mollist = []
+            realist = []
+            enzlist = []
+            cplxlist = []
+            for reItem in Neutral(meshEnt).getNeighbors('remeshReacs'):
+                if isinstance(element(reItem),moose.EnzBase):
+                    enzlist.append(reItem)
+                else:
+                    realist.append(reItem)
+                objInfo = reItem.path+'/info'
+                xcord.append(float(element(objInfo).getField('x')))
+                ycord.append(float(element(objInfo).getField('y')))
+                
+            for mitem in Neutral(meshEnt).getNeighbors('remesh'):
+                """ getNeighbors(remesh) has eliminating GSLStoich """
+                if isinstance(element(mitem[0].parent),CplxEnzBase):
+                    cplxlist.append(mitem)
+                    objInfo = (mitem[0].parent).path+'/info'
+
+                elif isinstance(element(mitem),moose.PoolBase):
+                    mollist.append(mitem)
+                    objInfo = mitem.path+'/info'
+                xcord.append(float(element(objInfo).getField('x')))
+                ycord.append(float(element(objInfo).getField('y')))
+
+            self.meshEntry[meshEnt] = {'enzyme':enzlist,
+                                       'reaction':realist,
+                                       'pool':mollist,
+                                       'cplx':cplxlist}
+            self.xmin = min(xcord)
+            self.xmax = max(xcord)
+            self.ymin = min(ycord)
+            self.ymax = max(ycord)
+
+            self.noPositionInfo = len(np.nonzero(xcord)[0]) == 0 \
+                                  and len(np.nonzero(ycord)[0]) == 0
+            
