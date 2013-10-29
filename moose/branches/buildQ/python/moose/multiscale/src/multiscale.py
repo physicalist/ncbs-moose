cf3d845ae35f5764c74b83678aed7365f5abc7ae
Dilawar Singh <dilawar.s.rajput@gmail.com>
2013-10-29 04:47:39 +0000

    Added a figure. It shows how we are bringing in sqlite3 database into between to make the transformation of XML to moose-datastructure. The transforamtion must be well-defined and smooth. And the user must not get error if his XML are correct. Generate moose scripts and do not map directly to moose objects.
    updated abstract
    
    Adding chunk informations
    
    Adding chunks and their index in main file
    
    Fixed an error in pynoweb
    
    Fixed paths
    
    go back to room and come back in morning.
    
    Updated figure

--- moose/branches/buildQ/python/moose/multiscale/src/multiscale.py
+++ moose/branches/buildQ/python/moose/multiscale/src/multiscale.py
@@ -7,13 +7,25 @@ import debug
 logger = logging.getLogger('multiscale')
 from lxml import etree
 
-
+import sqlite3 as sql 
 
 class Multiscale :
   
   def __init__(self, xmlDict) :
     self.xmlDict = xmlDict 
-    
+    self.dbdir = 'db'
+    self.dbname = 'models.db'
+    self.dbpath = os.path.join(self.dbdir, self.dbname)
+    if not os.path.exists(self.dbpath) :
+      try :
+        os.makedirs(self.dbdir)
+      except Exception as e :
+        debug.printDebug("ERROR"
+            , "Faild to create directory {0} with error {1}".format(self.dbdir, e))
+        sys.exit(0)
+    self.conn = sql.connect(self.dbpath)
+    self.cursor = self.conn.cursor()
+    debug.printDebug("INFO", "Object of class Multiscale intialized ...")
 
   
 
@@ -21,6 +33,18 @@ class Multiscale :
   def buildMultiscaleModel(self) :
       debug.printDebug("INFO", "Starting to build multiscale model")   
       
+
+
   
+  def exit(self) :
+    # Clean up before you leave
+    self.cursor.commit()
+    self.conn.close()
+    sys.exit()
+
+  # Write down the tests, whenever needed.
   
+
+
+
 
