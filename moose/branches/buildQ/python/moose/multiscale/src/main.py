7919908dde7b6c52f8d35f222bacb23ce419fdfb
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-10-29 08:59:09 +0000

    Changed the command line parser to accept 6 type of XML models. NeuroML is must and one must supply a adaptorML model also.
    Changed the argparser, Added nargs. Now arguments are returned as a list and one can iterate over them to tranverse them. Need to fix the document.
    
    Changed the parser. it now accept 6 type of models on command line + 1 adaptor. Neuroml and adaptor must be passed

--- moose/branches/buildQ/python/moose/multiscale/src/main.py
+++ moose/branches/buildQ/python/moose/multiscale/src/main.py
@@ -9,15 +9,20 @@ from lxml import etree
 
 def ifPathsAreValid(paths) :
   ''' Verify if path exists and are readable. '''
-  if paths.xml :
-    for path in paths.xml :
-      if os.path.isfile(path) : pass
-      else :
-        debug.printDebug("ERROR", "Filepath {0} does not exists".format(path))
-        return False
-    # check if file is readable 
-    if not os.access(path, os.R_OK) :
-      debug.printDebug("ERROR", "File {0} is not readable".format(path))
+  if paths :
+    paths = vars(paths)
+    for p in paths :
+      if not paths[p] : continue
+      for path in paths[p] :
+        if not path : continue
+        if os.path.isfile(path) : pass
+        else :
+          debug.printDebug("ERROR"
+            , "Filepath {0} does not exists".format(path))
+          return False
+      # check if file is readable 
+      if not os.access(path, os.R_OK) :
+        debug.printDebug("ERROR", "File {0} is not readable".format(path))
   return True
 
 # standard module for building a command line parser.
@@ -25,19 +30,40 @@ import argparse
 
 # This section build the command line parser
 argParser = argparse.ArgumentParser(description= 'Mutiscale modelling of neurons')
-argParser.add_argument('--xml', metavar='nmlpath'
+argParser.add_argument('--nml', metavar='nmlpath'
     , required = True
     , nargs = '+'
-    , help = 'multiscale model in XML'
+    , help = 'nueroml model'
     )
-argParser.add_argument('--adaptor', metavar='nmlpath'
+argParser.add_argument('--sbml', metavar='nmlpath'
+    , nargs = '*'
+    , help = 'sbml model'
+    )
+argParser.add_argument('--mechml', metavar='mechml'
+    , nargs = '*'
+    , help = 'mechml model'
+    )
+argParser.add_argument('--chml', metavar='channelml'
+    , nargs = '*'
+    , help = 'Channelml model'
+    )
+argParser.add_argument('--3dml', metavar='3dml'
+    , nargs = '*'
+    , help = '3DMCML model'
+    )
+argParser.add_argument('--meshml', metavar='meshml'
+    , nargs = '*'
+    , help = 'MeshML model'
+    )
+argParser.add_argument('--adaptor', metavar='adaptor'
     , required = True
+    , nargs = '+'
     , help = 'AdaptorML for moose'
     )
 args = argParser.parse_args()
 
 import parser
-if args.xml : 
+if args : 
   if ifPathsAreValid(args) :
     logger.info("Started parsing XML models")
     debug.printDebug("INFO", "Started parsing XML models")
