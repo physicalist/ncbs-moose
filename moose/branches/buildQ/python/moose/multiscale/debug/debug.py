caf6db26bf30848d326e62d4f009266ebf60f8a4
Dilawar Singh <dilawar.s.rajput@gmail.com>
2013-11-12 13:41:13 +0000

    Removing literate code.
    Removing literate code. Not good for python.
    
    Removing literate code

--- moose/branches/buildQ/python/moose/multiscale/debug/debug.py
+++ moose/branches/buildQ/python/moose/multiscale/debug/debug.py
@@ -1,33 +1,23 @@
-#!/usr/bin/env python
-
-# Filename       : debug.py
-# Created on     : Fri 06 Sep 2013 03:25:01 PM IST
-# Author         : Dilawar Singh
-# Email          : dilawars@ncbs.res.in
-#
-# Description    :
-# 
-# This file defines function which can be used to print in colors. Very useful
-# in debugging and printing on console.
-#
-# Logs           :
-
+from __future__ import print_function
 import inspect 
+import sys
 
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
 OKGREEN = '\033[92m'
 WARNING = '\033[93m'
-ERR = '\033[91m'
+ERR = '\033[31m'
 ENDC = '\033[0m'
 RED = ERR
 WARN = WARNING
-INFO = OKGREEN 
-TODO = OKBLUE
+INFO = OKBLUE
+TODO = OKGREEN
 DEBUG = HEADER
+ERROR = ERR
 
 prefix = dict(
     ERR = ERR
+    , ERROR = ERR
     , WARN = WARN
     , FATAL = ERR
     , INFO = INFO
@@ -36,7 +26,7 @@ prefix = dict(
     , DEBUG = DEBUG
     )
 
-def colored(msg, label) :
+def colored(msg, label="INFO") :
     """
     Return a colored string. Formatting is optional.
     """
@@ -45,7 +35,18 @@ def colored(msg, label) :
         color = prefix[label]
     else :
         color = ""
-    return "[{0}] {1} {2}".format(label, color+msg, ENDC)
+    return "{0} {1}".format(color+msg, ENDC)
 
-def printDebug(label, msg):
-    print(colored(msg, label))
+def printDebug(label, msg, frame=None):
+    if not frame :
+      print("[{0}] {1}".format(label, colored(msg,label)), file=sys.stderr)
+    else :
+      filename = frame.f_code.co_filename 
+      filename = "/".join(filename.split("/")[-2:])
+      print("[{3}] @...{0}:{1} {2}".format(filename
+        , frame.f_lineno
+        , colored(msg, label)
+        , label)
+        , file=sys.stderr
+        )
+
