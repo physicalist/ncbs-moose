d26b6582684470f50c19b88613962039d3f5ec00
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2013-11-13 14:03:35 +0000

    We can add segments of cell and attach some properties to them

--- moose/branches/buildQ/python/moose/multiscale/core/multiscale.py
+++ moose/branches/buildQ/python/moose/multiscale/core/multiscale.py
@@ -32,54 +32,126 @@ class Multiscale :
     self.model = moose.Neutral(self.rootModelPath)
     debug.printDebug("INFO", "Object of class Multiscale intialized ...")
 
-  def neuroml2Network(self, test=False) :
+  def neuroml2Network(self, nmls, test=False) :
     debug.printDebug("INFO", "Converting neuroml to netowrk graph")
-    nmlElems = self.xmlDict['nml']
-    if len(nmlElems) > 1 :
-      debug.printDebug("ERR", "More than one nueroml file is not supported yet.")
-      self.exit()
-    else : 
-      nmlElem, nmlPath = nmlElems[0]
-    self.nml = nmlElem
-    networkElem = xml_helper.getElement(self.nml, self.xmlExpr, "network")
-    if not  networkElem :
-      debug.printDebug("WARN", "No network found in model. Assuming a single"+
-        " cell model. ", inspect.currentframe())
-      cellList = xml_helper.getElement(self.nml, self.xmlExpr, "cell")
-      for cellXml in cellList :
-        self.insertCellIntoNetwork(cellXml) 
-    else :
-      self.networkG = nx.MultiDiGraph()
-      for network in networkElem :
-        networkId = network.get('id')
-        for nw in network :
-          if xml_helper.isTaggedWith(nw, "population") :
-            try :
-              populationId = nw.get('id')
-              if 'cell' in nw.keys() :
-                typeOfPopulation = 'cell'
-                populationId = nw.get('cell')
-              elif 'network' in nw.keys() :
-                typeOfPopulation = 'network'
-                populationId = nw.get('network')
-              elif 'component' in nw.keys() :
-                debug.printDebug("DEVL", "Component in network is not implmeneted yet.")
-              else : pass
-              print typeOfPopulation, populationId 
-            except :
-              debug.printDebug("WARN", "Failed adding network to networkG")
-              print(sys.exc_info())
-                
-
-          else :
-            debug.printDebug("WARN", "{0} not supported yet.".format(nw.tag))
-
-
-
-          
-
+    assert len(nmls) == 1
+    for nmlElem, nmlPath in nmls :
+      for elem in nmlElem.getroot() :
+        if xml_helper.isTaggedWith(elem, "cells") :
+          for cell in elem :
+            print self.cellToGraph(cell)
+        elif xml_helper.isTaggedWith(elem, "populations") :
+          print "Population of cell is found"
+          for population in elem :
+            self.insertInstanceOfCell(population)
+        else :
+          debug.printDebug("WARN"
+              , "Not supported yet {0}".format(elem))
     debug.printDebug("INFO", "Dumping graph into a dot file")
     graph_helper.write_dot(self.mooseG, 'graphs/moose.dot')
+
+
+  def cellToGraph(self, cell) :
+    '''
+    Convert a cell to graph. 
+    '''
+    cellName = cell.get('name')
+    cellG = nx.DiGraph(name=cellName)
+    for a in cell :
+      if xml_helper.isTaggedWith(a, "segments") :
+        for segment in a :
+          # Segment has a id, name and cable id. Cable is essentially
+          # segmentGroup in nml2. If no parent is specified then segment is
+          # parent.
+          segId = segment.get('id')
+          segName = segment.get('name')
+          nodeId = (cellName, segId)   # This will be unique
+          cellG.add_node(nodeId)
+
+          # Empty bio-physical properties.
+          cellG.node[nodeId]['name'] = cellName
+          cellG.node[nodeId]['biophysics'] = dict()
+
+          if segment.get('cable') :
+            cableId = segment.get('cable') 
+            cellG.node[nodeId]['cable'] = cableId
+          if segment.get('parent') :
+            # Add an edge from parent to child
+            parentNodeId = (cellName, segment.get('parent'))
+            cellG.add_edge(parentNodeId, nodeId)
+          # Attach proximal and distal information.
+          for ch in segment :
+            if xml_helper.isTaggedWith(ch, "proximal") :
+              cellG.node[nodeId]['proximal'] = ch
+            elif xml_helper.isTaggedWith(ch, "distal") :
+              cellG.node[nodeId]['distal'] = ch
+            else :
+              debug.printDebug("WARN"
+                  , "This element {0} is not supported yet".format(ch.tag)
+                  , frame=inspect.currentframe())
+      elif xml_helper.isTaggedWith(a, "notes") : pass
+      elif xml_helper.isTaggedWith(a, "cables") :
+        for cable in a :
+          cableId = cable.get('id')
+          cableName = cable.get('name')
+          # Also find the name of groups this cable can belong to.
+          groups = [x.text for x in cable.xpath("./*[local-name()='group']")]
+          # Find the nodes which are part of this cable and attach the cable to
+          # these nodes.
+          for n in cellG.nodes() :
+            if cellG.node[n]['cable'] == cableId :
+              # This cable (and this node) belongs to that many groups.
+              cellG.node[n]['belongsTo'] = groups
+            else : pass
+      elif xml_helper.isTaggedWith(a, "biophysics") :
+        for elem in a :
+          if xml_helper.isTaggedWith(elem, "mechanism") :
+            mechName = elem.get('name')
+            mechType = elem.get('type')
+            mech = (mechType, mechName)
+            params = elem.xpath("./*[local-name()='parameter']")
+            for p in params :
+              # This mechanism belong to many groups of cable. We have already
+              # attached groups to segments. Get these groups and attach these
+              # mechanism to nodes.
+              pName = p.get('name')
+              pValue = p.get('value')
+              grps = [x.text for x in p.xpath("./*[local-name()='group']")]
+              for g in grps :
+                for n in cellG.nodes() :
+                  if g in cellG.node[n]['belongsTo'] :
+                    cellG.node[n]['biophysics'][mech] = p
+            varParams = elem.xpath("./*[local-name()='variable_parameter']")
+            if varParams :
+              debug.printDebug("TODO", "Variable parameters are not implemented"
+                  , frame=inspect.currentframe())
+
+          else :
+            debug.printDebug("TODO", "Not implemented : {0}".format(elem)
+                , frame=inspect.currentframe())
+      else :
+        debug.printDebug("WARN", "{0} not supported yet".format(a)
+            , frame=inspect.currentframe())
+    return cellG
+
+  def insertInstanceOfCell(self, p) :
+    '''
+    Insert a instance of cell into the network of cell; the graph is called
+    cellG.
+    '''
+    instanceOf = p.get('cell_type')
+    name = p.get('name')
+    for cell in p :
+      if xml_helper.isTaggedWith(cell, "properties") :
+        print "Properties are found in cell"
+      elif xml_helper.isTaggedWith(cell, "instances") :
+        print "Instance of cell is found."
+      else :
+        debug.printDebug("WARN"
+            , "This element {0} is not supported yet.".format(cell.tag))
+
+
+ 
   def insertCellIntoNetwork(self, cell, test=False) :
     '''
     This function takes a XML representation of cell and insert in onto a graph.
@@ -151,28 +223,19 @@ class Multiscale :
         debug.printDebug("WARN", "Element {0} not supported yet.".format(elem.tag)
           , inspect.currentframe())
 
-  def attachToCompartment(self, cellId, xmlElem) :
-    debug.printDebug("INFO", "Attaching properties to element")
-     
 
   # This is the entry point of this class.
   def buildMultiscaleModel(self) :
       debug.printDebug("INFO", "Starting to build multiscale model")   
-      # NeuroML to moose
-      self.neuroml2Network(test=False)
+      if self.xmlDict['nml'] :
+        self.neuroml2Network(self.xmlDict['nml'], test=False)
 
 
-
-
-
-  
   def exit(self) :
     # Clean up before you leave
     debug.printDebug("WARN", "Cleaning up before existing..")
-    
     sys.exit(0)
 
-  # Write down the tests, whenever needed.
   
 
 
--- moose/branches/buildQ/python/moose/multiscale/run.sh
+++ moose/branches/buildQ/python/moose/multiscale/run.sh
@@ -2,7 +2,7 @@
 
 runCode() {
   python main.py \
-    --nml ./examples/NML2_FullNeuroML.nml \
+    --nml ./models/neuroml/v1.8/Cerebellum.xml \
     --adaptor ./schema/adaptor/adaptor.xml \
     --mumbl ./schema/mumbl/mumbl.xml
   neato -Teps graphs/moose.dot > graphs/moose.eps
