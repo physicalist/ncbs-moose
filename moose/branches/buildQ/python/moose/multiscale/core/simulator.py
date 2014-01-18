cee55aeff04dc626304e45f43b953fabfe50af75
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-01-18 10:46:38 +0000

    Basic framework is ready.

--- moose/branches/buildQ/python/moose/multiscale/core/simulator.py
+++ moose/branches/buildQ/python/moose/multiscale/core/simulator.py
@@ -3,7 +3,7 @@
 """simulator.py:  This class reads the variables needed for simulation and
 prepare moose for simulation.
 
-Last modified: Fri Jan 10, 2014  05:43PM
+Last modified: Sat Jan 18, 2014  01:49PM
 
 """
 
@@ -26,6 +26,7 @@ import os
 import logging 
 import core.config as config
 import numpy as np
+import logging
 
 class Simulator:
 
@@ -43,21 +44,25 @@ class Simulator:
         self.plotDt = float(self.globalVar.get('plot_dt'))
         self.simMethod = self.globalVar.get('sim_method')
         self.simulate = True
+        self.totalPlots = 0
+        self.currentCompartmentPath = ''
+
+        self.figurePath = self.rootElem.get('save_path', '.')
+        if not os.path.isdir(self.figurePath):
+            os.makedirs(self.figurePath)
 
         if self.rootElem.get('simulate') == "false":
             self.simulate = False
         if self.rootElem.get('runtime'):
             self.runtime = float(self.rootElem.get('runtime'))
         else:
-            debug.printDebug("INFO"
-                    , "No run-time for simulation is given. Using default 10.0"
+            logging.info( "No run-time for simulation is given." +
+                    " Using default 10.0"
                     )
             self.runtime = 10.0
         if self.elecPath is not None:
             self.elecPath = '/neuroml/electrical'
-            debug.printDebug("INFO"
-                    , "Using default library path %s" % self.elecPath 
-                    )
+            logging.info("Using default library path %s" % self.elecPath)
 
  
     def updateMoose(self, populationDict, projectionDict):
@@ -65,9 +70,6 @@ class Simulator:
         """
         self.popDict = populationDict
         self.projDict = projectionDict
-
-        # Fist setup simulation time  and plot variable etc.
-        # TODO: Add them here.
 
         # Add simulation element e
         [self.mapSimulationElement(x) for x in 
@@ -82,18 +84,14 @@ class Simulator:
             # Now get all records and setup moose for them.
             records = xmlElem.findall('record')
             if records:
-                [ self.setupRecored(x, xmlElem.attrib) for x in records ]
+                [ self.setupRecord(x, xmlElem.attrib) for x in records ]
             else: 
-                debug.printDebug("WARN"
-                    , "No record elements found in config.xml file." 
-                    )
+                logging.warning("No record elements found in config.xml file.")
+                return 
         else:
-            debug.printDebug("TODO"
-                    , "Not implemented {0}".format(simXml)
-                    , frame = inspect.currentframe()
-                    )
+            logging.warning("Not implemented {0}".format(xmlElem))
 
-    def setupRecored(self, recordXml, params):
+    def setupRecord(self, recordXml, params):
         """
         Params is a dictionary having attributes to <element>
         """
@@ -115,9 +113,9 @@ class Simulator:
         
         try:
             targetBasePath = self.popDict[populationType][1][instanceId].path
-            debug.printDebug("INFO", "Target path is %s " % targetBasePath)
+            logging.info("Target path is %s " % targetBasePath)
         except KeyError as e:
-            debug.printDebug("ERR", "Key {0} not found".format(populationType))
+            logging.debug("Key {0} not found".format(populationType))
             print("\t|- Available population in this model are following. ")
             print("\t+ {0} -| ".format(self.popDict.keys()))
             raise UserWarning, "Unknown population type."
@@ -214,100 +212,153 @@ class Simulator:
             moose.start(self.runtime)
 
             # After simulation, plot user-requested variables.
-            [ self.addPlot(pXml) for pXml in variablesToPlot ] 
+            if recordXml.get('single_plot', 'false') == 'true':
+                self.plotAllOnOne(variablesToPlot)
+            else:
+                [self.addPlot(pXml) for pXml in variablesToPlot] 
+
+    def plotAllOnOne(self, variables):
+        """
+        Plot all plots on one file
+        """
+        figname = os.path.join(self.figurePath
+                , ''.join([y for x, y, z in variables])
+                )+'.eps'
+        logging.info("Saving all plots to %s" % figname)
+
+        self.totalPlots += len(variables)
+
+        for i, var in enumerate(variables):
+            varType, varName, plotXml = var
+            varName = varType + varName
+            assert varName in self.tableDict.keys()
+            configDict = self.configurePlot(plotXml)
+            yvec, xvec = self.getPlotData(varName)
+            pylab.subplot(self.totalPlots, 1, i)
+            self.plotVar(xvec, yvec, configDict)
+        if figname:
+            pylab.savefig(figname)
+        else:
+            pylab.show()
+    
+    def plotVar(self, xvec, yvec, plotParams):
+
+        plotArgs = plotParams.get('plot_args', '')
+        pylab.plot(xvec, yvec, plotArgs)
+        pylab.xlabel(plotParams.get('xlabel', ''), fontsize=8)
+        pylab.ylabel(plotParams.get('ylabel', ''), fontsize=8)
+        pylab.title(plotParams.get('title', ''), fontsize=8)
+        pylab.tick_params(labelsize='6')
+        
+    def configurePlot(self, plotXml):
+        '''
+        Configure plot according to plotXml
+        '''
+        plotType = plotXml.get('type', '2d')
+        if plotType == "2d":
+            return self.configure2DPlot(plotXml)
+        else:
+            logging.warning("Plot type %s is not supported yet." % plotType)
+            return dict()
+        
+
+    def configure2DPlot(self, plotXml):
+        """
+        Configure 2D plots
+        """
+
+        plotParams = dict()
+        xXml = plotXml.find('x')
+        yXml = plotXml.find('y')
+        try:
+            self.start = float(xXml.attrib['start'])
+        except Exception as e:
+            self.start = 0.0
+        assert self.start >= 0.0, "Must start at >= 0.0 time. Right?"
+
+        try:
+            self.step = float(xXml.attrib['step'])
+        except Exception as e:
+            try:
+                self.step = self.simDt 
+            except Exception as e:
+                self.step = 10e-6
+                logging.warning(
+                        "No step size specified. Using {0}".format(self.step)
+                        )
+        if self.step <= 0.0:
+            logging.critical("Non-positive step size.  {0}".format(self.step))
+            raise RuntimeError, "Non-positive step size"
+            
+        try:
+            self.stop = float(xXml.attrib['stop'])
+        except Exception as e:
+            self.stop = self.runtime
+            logging.debug(
+                    "No stop time is specified. Using default %s" % self.stop
+                    )
+        assert self.stop > 0.0
+
+        # Label and title
+        labelXml = plotXml.find('label')
+        plotParams['output_file'] = plotXml.get('output', None)
+        if labelXml is not None:
+            plotParams['xlabel'] = labelXml.get('x')
+            plotParams['ylabel'] = labelXml.get('y')
+            plotParams['title'] = "{}:{}".format(
+                    self.currentCompartmentPath
+                    , ':' 
+                    , labelXml.get('title', '')
+                    )
+
+        return plotParams
+
+    def getPlotData(self, varName):
+        """Get the plot data from dictionary
+        """
+        assert varName in self.tableDict.keys()
+        tableObj, self.currentCompartmentPath = self.tableDict[varName]
+        
+        # If there is nothing to plot, quit
+        if len(tableObj.vec) == 0:
+            logging.warning("Empty vec. Nothing to plot.")
+            return None, None
+
+        xvec = numpy.arange(self.start, self.stop*2, self.step)
+
+        # Resize xvec to fit the simulation size.
+        logging.debug(
+                "Resizing vector of {} to fit in simulation size {}".format(
+                    varName
+                    , tableObj.vec.size
+                    )
+                )
+        xvec = xvec[ : tableObj.vec.size ]
+        return tableObj.vec, xvec
 
     def addPlot(self, varsToPlot):
         """
         Draws given variables.
         """
 
-        varType, variableName, plotXml = varsToPlot
-        varsToPlotName = varType + variableName 
+        varType, varName, plotXml = varsToPlot
+        varName = varType + varName 
 
-        # We must get a table object. If object is not available the program
-        # must terminate here.
-        assert varsToPlotName in self.tableDict.keys()
-        tableObj, path = self.tableDict[varsToPlotName]
+        plotParams = self.configurePlot(plotXml)
+        yvec, xvec = self.getPlotData(varName)
 
-        debug.printDebug("INFO"
-                , "Adding plotXml for {0}".format(varsToPlotName)
-                )
-        if plotXml.get('type') is None or plot.get('type') == "2d":
-            xXml = plotXml.find('x')
-            yXml = plotXml.find('y')
-            try:
-                start = float(xXml.attrib['start'])
-            except Exception as e:
-                start = 0.0
-            assert start >= 0.0
+        if xvec is None or yvec is None:
+            return
+        if plotXml.get('plot', 'true') == "false":
+            return 
 
-            try:
-                step = float(xXml.attrib['step'])
-            except Exception as e:
-                try:
-                    step = self.simDt 
-                except Exception as e:
-                    step = 10e-6
-                    debug.printDebug("WARN"
-                            , "No step size specified. Using {0}".format(step)
-                            )
-            if step <= 0.0:
-                debug.printDebug("ERR", "Non-positive step size.  {0}".format(
-                    step))
-                raise RuntimeError, "Non-positive step size"
-                
-            try:
-                stop = float(xXml.attrib['stop'])
-            except Exception as e:
-                stop = self.runtime
-                debug.printDebug("WARN"
-                        , "No stop time is specified. Using default %s" % stop
-                        )
-            assert stop > 0.0
-
-            xvec = numpy.arange(start, stop*2, step)
-
-            # Resize xvec to fit the simulation size.
-            xvec = xvec[ : tableObj.vec.size ]
-
-            if plotXml.get('plot') == "false":
-                return 
-
-            if plotXml.get('output'):
-                filename = plotXml.get('output')
-
-            if plotXml.get('plot_args') is None:
-                plotArgs = ''
-            else:
-                plotArgs = plotXml.get('plot_args')
-            pylab.figure()
-
-            # If there is nothing to plot, quit
-            if len(tableObj.vec) == 0:
-                logging.warning("Empty vec. Nothing to plot.")
-                return
-
-            debug.printDebug("DEBUG", "Plot args: {0}".format(plotArgs))
-            print(tableObj.vec)
-            pylab.plot(xvec, tableObj.vec, plotArgs)
-            
-            label = plotXml.find('label')
-            if label is not None:
-                pylab.xlabel(label.get('x'))
-                pylab.ylabel(label.get('y'))
-                pylab.title(path + ':' + label.get('title', ' '))
-
-            outputFile = plotXml.get('output')
-            if outputFile is not None and len(outputFile.strip()) > 0:
-                debug.printDebug("USER"
-                        , "Saving figure to {0}".format(outputFile)
-                        )
-                pylab.savefig(outputFile.strip())
-            else:
-                pylab.show()
-            
-
-        elif plotXml.get('type') in ["3d", "3D"]:
-            debug.printDebug("TODO", "3d plotXmlting is not implemented yet.")
-            raise UserWarning, "Feature not implemented"
+        self.plotVar(xvec, yvec, plotParams)
+        outputFile = plotParams.get('output_file', None)
+        if outputFile:
+            debug.printDebug("USER"
+                    , "Saving figure to {0}".format(outputFile)
+                    )
+            pylab.savefig(outputFile.strip())
+        else:
+            pylab.show()
 
