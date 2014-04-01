e196383b43bee807c99da86fdd1bb37f6c57df01
Dilawar Singh <dilawar.s.rajput@gmail.com>
2014-04-01 13:00:17 +0000

    Writing tests in cmake. sanity test: load_moose_into_python. Failing...

---
+++ moose/branches/async_multiscale_cmake/python/moose/test.py
@@ -0,0 +1,11 @@
+try:
+    import _moose
+except Exception as e:
+    print("Failed to import _moose module with error {}".format(e))
+    raise e
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
