14aae8a7a92e1f28f0953be9fb22030821bd19a3
Dilawar Singh <dilawar.s.rajput@gmail.com>
2014-04-03 10:17:19 +0000

    Segfault at HinesMatrix.cpp:143.

--- moose/branches/async_multiscale_cmake/hsolve/HSolveUtils.h
+++ moose/branches/async_multiscale_cmake/hsolve/HSolveUtils.h
@@ -10,124 +10,132 @@
 #ifndef _HSOLVE_UTILS_H
 #define _HSOLVE_UTILS_H
 
+#include "header.h"
+#include "../external/debug/print_function.h"
+
+#include "../biophysics/HHGate.h"
+#include "../biophysics/ChanBase.h"
+#include "../biophysics/HHChannel.h"
+
+#include "../basecode/OpFunc.h"
+
+
 class HSolveUtils
 {
 public:
-	static void initialize( Id object );
-#ifdef  OLD_API
-        
-            template< class T, class A >
-            static void set( Id id, string field, A value )
-            {
-                const Finfo* finfo = id()->cinfo()->findFinfo( "set_" + field );
-                assert( finfo );
+    static void initialize( Id object );
 
-                const DestFinfo* dest = dynamic_cast< const DestFinfo* >( finfo );
-                assert( dest );
+    template< class T, class A >
+    static void set( Id id, string field, A value )
+    {
+        const Finfo* finfo = id.element()->cinfo()->findFinfo( "set_" + field );
+        assert( finfo );
 
-                const OpFunc* op = dest->getOpFunc();
-                assert( op );
+        const DestFinfo* dest = dynamic_cast< const DestFinfo* >( finfo );
+        assert( dest );
 
-                Qinfo* qDummy = NULL;
-                const double* vp = reinterpret_cast< const double* >( &value );
-                op->op( id.eref(), qDummy, vp );
-            }
+        const OpFunc* op = dest->getOpFunc();
+        assert( op );
 
-        template< class T, class A >
-            static A get( Id id, string field )
-            {
-                const Finfo* finfo = id()->cinfo()->findFinfo( "get_" + field );
-                assert( finfo );
+        const double* vp = reinterpret_cast< const double* >( &value );
 
-                const DestFinfo* dest = dynamic_cast< const DestFinfo* >( finfo );
-                assert( dest );
+        /* NOTE: This is modified by Dilawar */
+        static_cast<OpFunc0<T>*>(op)->op( id.eref(), vp );
+    }
 
-                const OpFunc* op = dest->getOpFunc();
-                assert( op );
+    template< class T, class A >
+    static A get( Id id, string field )
+    {
+        const Finfo* finfo = id.element()->cinfo()->findFinfo( "get_" + field );
+        assert( finfo );
 
-                const GetOpFunc< T, A >* gop =
-                    dynamic_cast< const GetOpFunc< T, A >* >( op );
-                const GetEpFunc< T, A >* gep =
-                    dynamic_cast< const GetEpFunc< T, A >* >( op );
-                assert( gop || gep );
+        const DestFinfo* dest = dynamic_cast< const DestFinfo* >( finfo );
+        assert( dest );
 
-                if ( gop )
-                    return gop->reduceOp( id.eref() );
-                else // gep
-                    return gep->reduceOp( id.eref() );
-            }
-#else      /* -----  not OLD_API  ----- */
-        
-#endif     /* -----  not OLD_API  ----- */
-	
-	static int adjacent( Id compartment, vector< Id >& ret );
-	static int adjacent( Id compartment, Id exclude, vector< Id >& ret );
-	static int children( Id compartment, vector< Id >& ret );
-	static int channels( Id compartment, vector< Id >& ret );
-	static int hhchannels( Id compartment, vector< Id >& ret );
-	static int gates( Id channel, vector< Id >& ret, bool getOriginals = true );
-	static int spikegens( Id compartment, vector< Id >& ret );
-	static int synchans( Id compartment, vector< Id >& ret );
-	static int leakageChannels( Id compartment, vector< Id >& ret );
-	static int caTarget( Id channel, vector< Id >& ret );
-	static int caDepend( Id channel, vector< Id >& ret );
-	
-	class Grid
-	{
-	public:
-		Grid( double min, double max, unsigned int divs )
-			:
-			min_( min ),
-			max_( max ),
-			divs_( divs )
-		{
-			dx_ = ( max_ - min_ ) / divs_;
-		}
-		
-		unsigned int size();
-		double entry( unsigned int i );
-		
-		bool operator ==( const Grid& other ) {
-			return
-				min_ == other.min_ &&
-				max_ == other.max_ &&
-				divs_ == other.divs_;
-		}
-		
-		double min_;
-		double max_;
-		unsigned int divs_;
-		double dx_;
-	};
-	
-	// Functions for accessing gates' lookup tables.
-	//~ static int domain(
-		//~ Id gate,
-		//~ double& min,
-		//~ double& max );
-	static void rates(
-		Id gate,
-		Grid grid,
-		vector< double >& A,
-		vector< double >& B );
-	//~ static int modes(
-		//~ Id gate,
-		//~ int& AMode,
-		//~ int& BMode );
-	
-	static int targets(
-		Id object,
-		string msg,
-		vector< Id >& target,
-		string filter = "",
-		bool include = true );
-	
-	static int targets(
-		Id object,
-		string msg,
-		vector< Id >& target,
-		const vector< string >& filter,
-		bool include = true );
+        const OpFunc* op = dest->getOpFunc();
+        assert( op );
+
+        const GetOpFunc< T, A >* gop =
+            dynamic_cast< const GetOpFunc< T, A >* >( op );
+        const GetEpFunc< T, A >* gep =
+            dynamic_cast< const GetEpFunc< T, A >* >( op );
+        assert( gop || gep );
+
+        if ( gop )
+            return gop->reduceOp( id.eref() );
+        else // gep
+            return gep->reduceOp( id.eref() );
+    }
+
+    static int adjacent( Id compartment, vector< Id >& ret );
+    static int adjacent( Id compartment, Id exclude, vector< Id >& ret );
+    static int children( Id compartment, vector< Id >& ret );
+    static int channels( Id compartment, vector< Id >& ret );
+    static int hhchannels( Id compartment, vector< Id >& ret );
+    static int gates( Id channel, vector< Id >& ret, bool getOriginals = true );
+    static int spikegens( Id compartment, vector< Id >& ret );
+    static int synchans( Id compartment, vector< Id >& ret );
+    static int leakageChannels( Id compartment, vector< Id >& ret );
+    static int caTarget( Id channel, vector< Id >& ret );
+    static int caDepend( Id channel, vector< Id >& ret );
+
+    class Grid
+    {
+    public:
+        Grid( double min, double max, unsigned int divs )
+            :
+            min_( min ),
+            max_( max ),
+            divs_( divs )
+        {
+            dx_ = ( max_ - min_ ) / divs_;
+        }
+
+        unsigned int size();
+        double entry( unsigned int i );
+
+        bool operator ==( const Grid& other )
+        {
+            return
+                min_ == other.min_ &&
+                max_ == other.max_ &&
+                divs_ == other.divs_;
+        }
+
+        double min_;
+        double max_;
+        unsigned int divs_;
+        double dx_;
+    };
+
+    // Functions for accessing gates' lookup tables.
+    //~ static int domain(
+    //~ Id gate,
+    //~ double& min,
+    //~ double& max );
+    static void rates(
+        Id gate,
+        Grid grid,
+        vector< double >& A,
+        vector< double >& B );
+    //~ static int modes(
+    //~ Id gate,
+    //~ int& AMode,
+    //~ int& BMode );
+
+    static int targets(
+        Id object,
+        string msg,
+        vector< Id >& target,
+        string filter = "",
+        bool include = true );
+
+    static int targets(
+        Id object,
+        string msg,
+        vector< Id >& target,
+        const vector< string >& filter,
+        bool include = true );
 };
 
 #endif // _HSOLVE_UTILS_H
