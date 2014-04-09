7736e7e0637e45d1cde26bc67315df79ca30b698
zbigniew_szmek <zbigniew_szmek@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-04-09 21:27:14 +0000

    Make sure classes have __module__ set
    Without this, help() does not work.

--- moose/branches/zbigniew_py3k/pymoose/moosemodule.cpp
+++ moose/branches/zbigniew_py3k/pymoose/moosemodule.cpp
@@ -2029,9 +2029,18 @@ extern "C" {
         if (base && !defineClass(module_dict, base)){
             return 0;
         }
+
+        string str = "moose." + className;
+
         PyTypeObject * new_class =
                 (PyTypeObject*)PyType_Type.tp_alloc(&PyType_Type, 0);
         // Py_TYPE(new_class) = &PyType_Type;
+#ifdef PY3K
+        new_class->tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HEAPTYPE;
+        PyHeapTypeObject* et = (PyHeapTypeObject*)new_class;
+        et->ht_name = PyUnicode_FromString(className.c_str());
+        et->ht_qualname = PyUnicode_FromString(str.c_str());
+#else
         new_class->tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE;
         /*
         should we avoid Py_TPFLAGS_HEAPTYPE as it imposes certain
@@ -2052,11 +2061,9 @@ extern "C" {
         heaptype is not set it uses tp_name to print the help.
         Py_SIZE(new_class) = sizeof(_ObjId);        
         */
-        string str = "moose." + className;
-        new_class->tp_name = (char *)calloc(str.length()+1,
-                                            sizeof(char));
-        strncpy(const_cast<char*>(new_class->tp_name), str.c_str(),
-                str.length());
+#endif
+        new_class->tp_name = strdup(str.c_str());
+
         new_class->tp_doc = moose_Class_documentation;
         // strncpy(new_class->tp_doc, moose_Class_documentation, strlen(moose_Class_documentation));
         map<string, PyTypeObject *>::iterator base_iter =
@@ -2114,7 +2121,9 @@ extern "C" {
             cout << "Created class " << new_class->tp_name << endl
                  << "\tbase=" << new_class->tp_base->tp_name << endl;
         }
-        // PyDict_SetItemString(new_class->tp_dict, "__module__", PyString_FromString("moose"));
+#ifdef PY3K
+        PyDict_SetItemString(new_class->tp_dict, "__module__", PyUnicode_InternFromString("moose"));
+#endif
         // string doc = const_cast<Cinfo*>(cinfo)->getDocs();
         // PyDict_SetItemString(new_class->tp_dict, "__doc__", PyString_FromString(" \0"));
         // PyDict_SetItemString(module_dict, className.c_str(), (PyObject *)new_class);
