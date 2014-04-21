58aca6cf1b2a6ad3ecedfe333d7177b419d310d7
Dilawar Singh <dilawar.s.rajput@gmail.com>
2014-04-21 11:16:20 +0000

    Temporary commit to transfer changes to a local machine.

--- moose/branches/async_multiscale_cmake/hsolve/CTestTestfile.cmake
+++ moose/branches/async_multiscale_cmake/hsolve/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/dilawar/Work/NCBS/moose_svn1.7/moose/branches/async_multiscale_cmake/hsolve
-# Build directory: /home/dilawar/Work/NCBS/moose_svn1.7/moose/branches/async_multiscale_cmake/hsolve
+# Source directory: /home/dilawar/Work/NCBS/moose_svn1.8/moose/branches/async_multiscale_cmake/hsolve
+# Build directory: /home/dilawar/Work/NCBS/moose_svn1.8/moose/branches/async_multiscale_cmake/hsolve
 # 
-# This file includes the relevent testing commands required for 
+# This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
