f26255820e960df11b37804de98ae99d191fcea5
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-04-26 07:01:55 +0000

    Added a script to merge async13 branch to this branch. Merged async13 branch to this branch. Compiles fine.

--- moose/branches/async_multiscale_cmake/merge_async13.sh
+++ moose/branches/async_multiscale_cmake/merge_async13.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Updaing async13 branch"
 ( cd ../async13/ && svn up )
-svn merge ../async13/
+if [ $# -lt 2]; then
+    echo "USAGE: ./$0 rev_no"
+    exit
+fi
+svn merge -c $1 ../async13
