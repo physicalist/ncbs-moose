c3ad842e1e6bce59f804f1d5de21821ccb4407a3
saeedsh <saeedsh@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-05-13 09:25:33 +0000

    OpenMP synchronization

--- moose/branches/async_gpu/hsolve/PN2S/PN2S_Device.h
+++ moose/branches/async_gpu/hsolve/PN2S/PN2S_Device.h
@@ -8,15 +8,32 @@
 #if !defined(AB1F38D16_FE78_4893_BC39_356BCBBCF345__INCLUDED_)
 #define AB1F38D16_FE78_4893_BC39_356BCBBCF345__INCLUDED_
 #include "Definitions.h"
-
+#include "core/PN2S_Solver.h"
+#include <omp.h>
 class PN2S_Device
 {
-
 public:
+	hscError SelectDevice();
+
+	int id;
+	double _dt;
+	vector<PN2S_ModelPack> _modelPacks;
+
 	PN2S_Device(int _id);
 	virtual ~PN2S_Device();
-	static int GetNumberOfActiveDevices();
-	hscError SelectDevice();
-	int id;
+
+	hscError PrepareSolver(vector<PN2SModel> &m,  double dt);
+	void Process();
+	hscError Setup();
+
+
+private:
+	int _queue_size;
+	void task1_prepareInput(omp_lock_t& _empty_lock,omp_lock_t& _full_lock, int& state);
+	void task2_DoProcess(omp_lock_t& _empty_lock_input,	omp_lock_t& _full_lock_input,omp_lock_t& _empty_lock_output, int& state);
+	void task3_prepareOutput(omp_lock_t& _empty_lock, int& state);
+
 };
+
+
 #endif // !defined(AB1F38D16_FE78_4893_BC39_356BCBBCF345__INCLUDED_)
