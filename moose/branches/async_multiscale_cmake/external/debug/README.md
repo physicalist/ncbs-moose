756a924a4a777c57361afd69c6ecf3b5b4325d53
Dilawar Singh <dilawar.s.rajput@gmail.com>
2014-05-20 17:45:29 +0000

    Renamed the library name. Made it header only.

--- moose/branches/async_multiscale_cmake/external/debug/README.md
+++ moose/branches/async_multiscale_cmake/external/debug/README.md
@@ -1,4 +1,89 @@
 simpletest
 ==========
 
-A simple framework for testing and logging framework in C++
+A simple framework for testing and logging framework in C++.
+
+TESTING:
+-------
+
+Have a look at `test1.cpp` file.
+
+~~~
+#include "simple_test.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* Following two macros takes two arguments: first is condition and second
+     * is message to print on console in case of failture
+     */
+    EXPECT_TRUE( 1 == 2, "1 == 2 must fail");
+
+    /* TO instroduce colors into user-messages, sorround them with ` */
+    EXPECT_TRUE( 1 == 2, "`1 == 2 must fail`");
+
+    EXPECT_FALSE( 2 != 2, "2 != 2 must also fail");
+
+
+    /* Following macros takes three argument and they try to mimic GTEST macros. */
+    EXPECT_EQ(2, 3, "This should fail");
+
+    /* This is colored. */
+    EXPECT_NEQ(2, 2, "`I was expecting not equal to 2`");
+
+    int expected = 3;
+    EXPECT_GTE(2, expected, "I was expecting greater than equal to" << expected );
+    EXPECT_GT(3, expected, "I was expecting greater than " << expected);
+
+    EXPECT_LTE(5, expected, "I was expecting less than equal to " << expected);
+    EXPECT_LT(3, expected, "I was expecting less than "<< expected);
+
+    /* If a marco begins with ASSERT_ rather than EXPECT_ then it throws an
+     * exception.
+     */
+    ASSERT_LT(3, expected, "I was expecting less than " << expected );
+
+    return 0;
+}
+
+~~~
+
+Following is its output.
+
+~~~
+Press ENTER or type command to continue
+[EXPECT_FAILURE] 1 == 2 must fail
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:34
+ + Expected 2, received 3
+ + This should fail
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:35
+ + Not expected 2
+ + I was expecting not equal to 2
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:38
+ + Expected greater than or equal to 2, received 3
+ + I was expecting greater than equal to3
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:39
+ + Expected greater than 3, received 3
+ + I was expecting greater than 3
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:42
+ + Expected less than 3, received 3
+ + I was expecting less than 3
+ + 
+[EXPECT_FAILURE] In function: int main() file: test1.cpp:47
+ + Expected less than 3, received 3
+  1 Write readme and get over with it.                                               
+ + I was expecting less than 3
+ + 
+[ASSERTION_FAILURE] I was expecting less than 3
+terminate called after throwing an instance of 'FatalTestFailure'
+/bin/bash: line 1: 30297 Aborted                 ./a.out
+
+~~~
+
+The colored output is not displayed in this log.
---
+++ moose/branches/async_multiscale_cmake/external/debug/simple_assert.hpp
@@ -0,0 +1,128 @@
+/* This file is a modified version of boost/assert.hpp file. I guess I have to
+ * release it under the same license
+ */
+#undef SIMPLE_ASSERT
+
+#if defined(SIMPLE_DISABLE_ASSERTS)
+
+# define SIMPLE_ASSERT(expr) ((void)0)
+
+#elif defined(SIMPLE_ENABLE_ASSERT_HANDLER)
+
+#include "current_function.hpp"
+#include <stringstream>
+
+namespace moose
+{
+  void assertion_failed(char const * expr,
+                        char const * function, char const * file, long line); // user defined
+} // namespace moose
+
+#define SIMPLE_ASSERT(expr) ((expr) \
+  ? ((void)0) \
+  : ::moose::assertion_failed(#expr, SIMPLE_CURRENT_FUNCTION, __FILE__, __LINE__))
+
+#else
+# include <assert.h> // .h to support old libraries w/o <cassert> - effect is the same
+# define SIMPLE_ASSERT(expr) assert(expr)
+#endif
+
+//--------------------------------------------------------------------------------------//
+//                                   SIMPLE_ASSERT_MSG                                   //
+//--------------------------------------------------------------------------------------//
+
+# undef SIMPLE_ASSERT_MSG
+
+#if defined(SIMPLE_DISABLE_ASSERTS) || defined(NDEBUG)
+
+  #define SIMPLE_ASSERT_MSG(expr, msg) ((void)0)
+
+#elif defined(SIMPLE_ENABLE_ASSERT_HANDLER)
+
+  #include "current_function.hpp"
+
+  namespace moose
+  {
+    void assertion_failed_msg(char const * expr, char const * msg,
+                              char const * function, char const * file, long line); // user defined
+  } // namespace moose
+
+  #define SIMPLE_ASSERT_MSG(expr, msg) ((expr) \
+    ? ((void)0) \
+    : ::moose::assertion_failed_msg(#expr, msg, SIMPLE_CURRENT_FUNCTION, __FILE__, __LINE__))
+
+#else
+  #ifndef SIMPLE_ASSERT_HPP
+    #define SIMPLE_ASSERT_HPP
+    #include <cstdlib>
+    #include <iostream>
+    #include "current_function.hpp"
+
+    //  IDE's like Visual Studio perform better if output goes to std::cout or
+    //  some other stream, so allow user to configure output stream:
+    #ifndef SIMPLE_ASSERT_MSG_OSTREAM
+    # define SIMPLE_ASSERT_MSG_OSTREAM std::cerr
+    #endif
+
+    namespace moose
+    { 
+      namespace assertion 
+      { 
+        namespace detail
+        {
+          inline void assertion_failed_msg(char const * expr, char const * msg, char const * function,
+            char const * file, long line)
+          {
+            SIMPLE_ASSERT_MSG_OSTREAM
+              << "***** Internal Program Error - assertion (" << expr << ") failed in "
+              << function << ":\n"
+              << file << '(' << line << "): " << msg << std::endl;
+			#ifdef UNDER_CE
+				// The Windows CE CRT library does not have abort() so use exit(-1) instead.
+				std::exit(-1);
+			#else
+				std::abort();
+			#endif
+          }
+        } // detail
+      } // assertion
+    } // detail
+  #endif
+
+  #define SIMPLE_ASSERT_MSG(expr, msg) ((expr) \
+    ? ((void)0) \
+    : ::moose::assertion::detail::assertion_failed_msg(#expr, msg, \
+          SIMPLE_CURRENT_FUNCTION, __FILE__, __LINE__))
+#endif
+
+//--------------------------------------------------------------------------------------//
+//                                     SIMPLE_VERIFY                                     //
+//--------------------------------------------------------------------------------------//
+
+#undef SIMPLE_VERIFY
+
+#if defined(SIMPLE_DISABLE_ASSERTS) || ( !defined(SIMPLE_ENABLE_ASSERT_HANDLER) && defined(NDEBUG) )
+
+# define SIMPLE_VERIFY(expr) ((void)(expr))
+
+#else
+
+# define SIMPLE_VERIFY(expr) SIMPLE_ASSERT(expr)
+
+/*-----------------------------------------------------------------------------
+ *                                  SIMPLE_WARN
+ *
+ *  This macro accepts a stringstream like object as its second argument. First
+ *  arguement is condition which must return a bool.
+ *-----------------------------------------------------------------------------*/
+#define SIMPLE_WARN(condition, msg) \
+    if(! (condition) ) { \
+        std::ostringstream ss; \
+        ss << endl << "[WARN] " << msg; \
+        std::cout << ss.str() \
+        << std::endl; \
+        std::cout << "\n\t|- " << "In function: " << SIMPLE_CURRENT_FUNCTION \
+            << "\n\t+ In file: " << __FILE__ << ":" << __LINE__ << std::endl; \
+    }
+
+#endif
