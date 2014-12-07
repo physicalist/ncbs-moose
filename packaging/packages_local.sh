d65e30b5672c17f9952c375fd33814f839151051
dilawars <dilawars@d1858a74-f715-0410-99c9-c66fe9cce3d6>
2014-12-07 12:43:38 +0000

    Major overhaul of packaging branch. Making sure that mere copying the branch builds it on open-build-service and launchpad.

--- packaging/packages_local.sh
+++ packaging/packages_local.sh
@@ -3,7 +3,7 @@ echo "This scripts creates various type of packages of moose to be distributed \
     locally"
 mkdir -p  _build
 cd _build/
-cmake ../moose_3.0.0/
+cmake ../moose_3.0.1/
 make 
 echo "Creating self-extracting package"
 cpack -G STGZ
